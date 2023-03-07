c1097f8058c2c9a234fe36b7059e77e63f13c430
piromix <4474927+piromix@users.noreply.github.com>
2023-03-07 20:29:44 +0900

    add: add path to load the config path.
    ~/.config/gptr directory was added to the config read path.
    
    ~/.config/gptrディレクトリも設定ファイルを読み込むパスに追加しました。

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,18 @@
-use std::{env, fs};
-use std::path::{PathBuf};
 use dirs::home_dir;
+use std::path::PathBuf;
+use std::{env, fs};
 
 fn get_current_directory() -> PathBuf {
     return env::current_exe().unwrap();
 }
 
 pub fn read_config(file_name: String) -> Option<String> {
-    read_config_in_home_dir(&file_name).or(read_config_from_config_dir_under_home_dir(&file_name)).or(read_config_in_exe_dir(&file_name))
+    read_config_in_home_dir(&file_name)
+        .or(read_config_from_gptr_dir_under_config_under_home_dir(
+            &file_name,
+        ))
+        .or(read_config_from_config_dir_under_home_dir(&file_name))
+        .or(read_config_in_exe_dir(&file_name))
 }
 
 fn read_config_in_home_dir(file_name: &String) -> Option<String> {
@@ -30,6 +35,18 @@ fn read_config_from_config_dir_under_home_dir(file_name: &String) -> Option<Stri
 
 fn read_config_in_exe_dir(file_name: &String) -> Option<String> {
     let config_path = get_current_directory().parent().unwrap().join(file_name);
+    if config_path.exists() && config_path.is_file() {
+        Some(fs::read_to_string(config_path).expect("unable to read config file"))
+    } else {
+        None
+    }
+}
+
+/**
+ * Read config from ~/.config/gptr
+ */
+fn read_config_from_gptr_dir_under_config_under_home_dir(file_name: &String) -> Option<String> {
+    let config_path = home_dir().unwrap().join(".config/gptr").join(file_name);
     if config_path.exists() && config_path.is_file() {
         Some(fs::read_to_string(config_path).expect("unable to read config file"))
     } else {

1586b748bbcf49d9311825001ff02711ec853918
piromix <4474927+piromix@users.noreply.github.com>
2023-03-09 09:55:42 +0900

    fix: Delete commented out code.

--- src/gpt.rs
+++ src/gpt.rs
@@ -1,9 +1,9 @@
 use reqwest;
+use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
 use reqwest_middleware;
-use reqwest::header::{AUTHORIZATION, CONTENT_TYPE, HeaderMap};
 use serde::{Deserialize, Serialize};
 
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 
 pub struct Client {
     http_client: reqwest_middleware::ClientWithMiddleware,
@@ -26,23 +26,11 @@ struct OpenAIRequest {
 impl Client {
     pub fn new() -> Self {
         Self {
-            http_client: reqwest_middleware::ClientBuilder::new(reqwest::Client::new())
-                // .with(http_cache_reqwest::Cache(http_cache_reqwest::HttpCache {
-                //     mode: http_cache_reqwest::CacheMode::Default,
-                //     manager: http_cache_reqwest::CACacheManager::default(),
-                //     options: None,
-                // }))
-                .build(),
+            http_client: reqwest_middleware::ClientBuilder::new(reqwest::Client::new()).build(),
         }
     }
 
     fn create_message(&self, messages: Vec<Message>) -> anyhow::Result<String> {
-        // let mut messages:Vec<Message> = vec![];
-        // messages.push(Message {
-        //     role: "user".to_string(),
-        //     content: prompt.clone(),
-        // });
-
         let data = OpenAIRequest {
             model: "gpt-3.5-turbo".to_string(),
             messages,
@@ -51,18 +39,15 @@ impl Client {
         return Ok(serde_json::to_string(&data).unwrap());
     }
 
-    pub async fn async_query(
-        &self,
-        prompt: Vec<Message>,
-        key: &String,
-    ) -> Result<String> {
+    pub async fn async_query(&self, prompt: Vec<Message>, key: &String) -> Result<String> {
         let message = self.create_message(prompt)?;
 
         let mut headers = HeaderMap::new();
         headers.insert(AUTHORIZATION, format!("Bearer {}", key).parse().unwrap());
         headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
 
-        let response = self.http_client
+        let response = self
+            .http_client
             .post("https://api.openai.com/v1/chat/completions")
             .headers(headers)
             .body(message)
@@ -74,9 +59,12 @@ impl Client {
 
     fn handle_response(self: &Self, response: serde_json::Value) -> Result<String> {
         match response["error"].as_object() {
-            None => {},
+            None => {}
             Some(_) => {
-                return Err(anyhow!("received an error from openAI: {}", response["error"]["message"].as_str().unwrap()));
+                return Err(anyhow!(
+                    "received an error from openAI: {}",
+                    response["error"]["message"].as_str().unwrap()
+                ));
             }
         }
 
